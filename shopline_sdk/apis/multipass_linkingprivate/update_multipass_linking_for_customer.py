f165999a017d9ca52f51ddba88fbb377c81e88b6
HsOjo <1134031392@qq.com>
2025-11-05 01:17:55 +0800

    [fix] 补充部分缺失 API

--- shopline_sdk/apis/multipass_linkingprivate/update_multipass_linking_for_customer.py
+++ shopline_sdk/apis/multipass_linkingprivate/update_multipass_linking_for_customer.py
@@ -7,36 +7,30 @@ from typing_extensions import Literal
 from ...exceptions import ShoplineAPIError
 
 # 导入需要的模型
-from ...models.server_error import ServerError
+from ...models.not_found_error import NotFoundError
 from ...models.unprocessable_entity_error import UnprocessableEntityError
 
 class Body(BaseModel):
     """请求体模型"""
-    tags: List[str]
-    """Product tags array 
-       商品標簽array"""
+    sub: Optional[str] = None
+    """New identifier mapping for customer, allow regex ^[a-zA-Z0-9.\-@+ _]+$"""
 
 class Response(BaseModel):
     """响应体模型"""
-    tags: Optional[List[str]] = None
+    message: Optional[str] = None
 
 async def call(
-    session: aiohttp.ClientSession, productId: str, body: Optional[Body] = None
+    session: aiohttp.ClientSession, customer_id: str, body: Optional[Body] = None
 ) -> Response:
     """
-    Update (replace) current product tags
+    Update multipass linking for customer
     
-    Replaces current product tags with input tags array. 
-     Each tag mush have at least 3 characters and at most 40 characters.
-     When update with invalid length of tags, the update will fail and the respond will be 422.
-     以新的標簽取代現有的商品標簽。
-     每一個商品標籤必須有最少3個字完及最多40個字完。
-     若需更新的商品標籤其中之一不符字完長度規範，更新不會成功及將回傳狀態碼422。
+    Update customer's active linking record used for multipass login
     
-    Path: PUT /products/{productId}/tags
+    Path: POST /multipass/customers/{customer_id}/linkings
     """
     # 构建请求 URL
-    url = f"products/{productId}/tags"
+    url = f"multipass/customers/{customer_id}/linkings"
 
     # 构建请求头
     headers = {"Content-Type": "application/json"}
@@ -45,21 +39,21 @@ async def call(
     json_data = body.model_dump(exclude_none=True) if body else None
 
     # 发起 HTTP 请求
-    async with session.put(
+    async with session.post(
         url, json=json_data, headers=headers
     ) as response:
         if response.status >= 400:
             error_data = await response.json()
+            if response.status == 404:
+                error = NotFoundError(**error_data)
+                raise ShoplineAPIError(
+                    status_code=404,
+                    error=error
+                )
             if response.status == 422:
                 error = UnprocessableEntityError(**error_data)
                 raise ShoplineAPIError(
                     status_code=422,
-                    error=error
-                )
-            if response.status == 500:
-                error = ServerError(**error_data)
-                raise ShoplineAPIError(
-                    status_code=500,
                     error=error
                 )
             # 默认错误处理
